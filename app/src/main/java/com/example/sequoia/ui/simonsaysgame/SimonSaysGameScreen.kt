304b246f8a765508102223fe8da644013499fad2
Inseo Kim <i32kim@uwaterloo.ca>
2022-06-23 16:59:32 -0400

    button color change

--- app/src/main/java/com/example/sequoia/ui/simonsaysgame/SimonSaysGameScreen.kt
+++ app/src/main/java/com/example/sequoia/ui/simonsaysgame/SimonSaysGameScreen.kt
@@ -1,14 +1,21 @@
 package com.example.sequoia.ui.simonsaysgame
 
+import android.view.MotionEvent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import com.example.sequoia.R
@@ -29,44 +36,45 @@ fun SimonSaysGameScreen() {
 @Composable
 fun DrawSimonSaysBoard() {
     Column(
-        modifier = Modifier.padding(32.dp), horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
 
         for (i in 0 until 3) {
             Row {
                 for (j in 0 until 3) {
-//                    val isLightSquare = i % 2 == j % 2
-//                    val squareColor =
-//                        if (isLightSquare) colorResource(id = R.color.games_txt_green) else colorResource(
-//                            id = R.color.game_button_background_dark_green
-//                        )
-//                    Box(
-//                        modifier = Modifier
-//                            .weight(1f)
-//                            .aspectRatio(1f)
-//                            .background(color = squareColor)
-//                    )
-                    val squareColor = colorResource(id = R.color.games_txt_green)
-
-                    Box(
-                        modifier = Modifier
-                            .weight(1f)
-                            .aspectRatio(1f)
-                            .border(
-                                border = BorderStroke(
-                                    1.dp,
-                                    color = colorResource(id = R.color.black)
-                                )
-                            )
-                            .background(color = squareColor)
-                    ) {
-                            
-                        }
-
-
+                    squarebutton()
                 }
             }
         }
+    }
+}
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun squarebutton() {
+    val squareColor = colorResource(id = R.color.games_txt_green)
+    val presscolor = colorResource(id=R.color.button_pressed)
+    
+    val color = remember{ mutableStateOf(squareColor)}
+    androidx.compose.material3.Button(onClick = { /*TODO*/ },
+        shape = RectangleShape,
+        modifier = Modifier
+            .padding(10.dp)
+            .size(100.dp, 100.dp)
+            .pointerInteropFilter {
+                                  when(it.action) {
+                                      MotionEvent.ACTION_DOWN -> {
+                                          color.value = presscolor
+                                      }
+                                      MotionEvent.ACTION_UP -> {
+                                          color.value = squareColor
+                                      }
+                                  }
+                                    true
+            },
+        colors = ButtonDefaults.buttonColors(containerColor = color.value)) {
+
     }
 }
