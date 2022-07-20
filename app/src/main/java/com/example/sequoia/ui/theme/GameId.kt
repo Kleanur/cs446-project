8be34daa656b3414b12febf7da5e0015c283e0e3
Ayushi Guha <a3guha@uwaterloo.ca>
2022-07-19 20:29:05 -0400

    score control foundation

--- app/src/main/java/com/example/sequoia/ui/theme/GameId.kt
+++ app/src/main/java/com/example/sequoia/ui/theme/GameId.kt
@@ -1,3 +1,4 @@
 package com.example.sequoia.ui.theme
 
 val gameIds = mapOf("None" to 0, "SimonSays" to 1, "PitchPerfect" to 2, "GotRhythm" to 3, "LicketySplit" to 4)
+val games = mapOf(1 to "SimonSays")
